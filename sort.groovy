ed183da4207356068941ed898ffebddc44f3e793
Chirag Awasthi <96935674+chiragawasthi17@users.noreply.github.com>
2023-04-13 00:05:45 +0530

    Create sort.groovy

---
+++ sort.groovy
@@ -0,0 +1,16 @@
+job('job dsl')
+{
+    description('this is our second job dsl using apache groovy')
+    scm
+    {
+        git("https://github.com/chiragawasthi17/bubble-sort-visualizer.git", 'master')
+    }
+    trigger
+    {
+        scm('* * * * *')
+    }
+    steps
+    {
+        shell('docker build -t web-app9 . && docker run -itd --name web-app9 -p 8889:5000 web-app9')
+    }
+}
